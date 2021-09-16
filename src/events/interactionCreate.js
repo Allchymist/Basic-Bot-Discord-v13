34d4127ec64ca9f13fe0413b8603b6b410226d42
iPh04 <77502759+iPh04@users.noreply.github.com>
2021-09-16 00:32:58 -0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,13 +1,13 @@
 module.exports = {
     name: "interactionCreate",
-    async execute(i, bot) {
+    async execute(i, client) {
         if (!i.isCommand()) return;
 
-        const command = bot.slash.get(i.commandName);
+        const command = client.commands.get(i.commandName);
         if (!command) return;
 
         try {
-            await command.execute(bot, i);
+            await command.execute(client, i);
         } catch (error) {
             console.error(error);
             await i.reply({ content: 'Ocorreu um Erro!', ephemeral: true });
