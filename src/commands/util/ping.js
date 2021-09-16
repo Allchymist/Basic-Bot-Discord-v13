235c9f9bb64664e0c3780b7773dfa04a4e9c8ab4
iPh04 <77502759+iPh04@users.noreply.github.com>
2021-09-16 00:33:17 -0300

    Update ping.js

--- src/commands/util/ping.js
+++ src/commands/util/ping.js
@@ -4,7 +4,7 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription("Informações sobre a latência!"),
-    async execute(bot, i) {
+    async execute(client, i) {
         await i.reply(`Latência do Servidor: **${Date.now() - i.createdTimestamp}ms**\nLatência da API: **${Math.round(client.ws.ping)}ms**`);
     }
 }
